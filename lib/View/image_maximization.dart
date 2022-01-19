17169f8e50b11339a12ce114b1d72405620a4026
Jeevan <jeevan.bk@dikshatech.com>
2022-01-19 16:04:27 +0530

    Final Commit

--- lib/View/image_maximization.dart
+++ lib/View/image_maximization.dart
@@ -3,8 +3,8 @@ import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
 class ImageMaximization extends StatefulWidget {
-  const ImageMaximization(this.Imageurl, {Key key}) : super(key: key);
-  final Imageurl;
+  const ImageMaximization(this.imageUrl, {Key key}) : super(key: key);
+  final imageUrl;
 
   @override
   _ImageMaximizationState createState() => _ImageMaximizationState();
@@ -22,9 +22,9 @@ class _ImageMaximizationState extends State<ImageMaximization> {
           scrollPhysics: const BouncingScrollPhysics(),
           builder: (BuildContext context, int index) {
             return PhotoViewGalleryPageOptions(
-              imageProvider: NetworkImage(widget.Imageurl.toString()),
+              imageProvider: NetworkImage(widget.imageUrl.toString()),
               initialScale: PhotoViewComputedScale.contained * 0.8,
-              heroAttributes: PhotoViewHeroAttributes(tag: widget.Imageurl),
+              heroAttributes: PhotoViewHeroAttributes(tag: widget.imageUrl),
             );
           },
           itemCount: 1,
